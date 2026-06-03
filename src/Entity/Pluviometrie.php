40b6e5e6209e9dee85477b04227a1a7baedbda61
arrese64 <anartz.arreseigor@gmail.com>
2026-06-03 11:41:28 +0200

    Update Pluviometrie.php

--- src/Entity/Pluviometrie.php
+++ src/Entity/Pluviometrie.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\PluviometrieRepository;
 use Doctrine\ORM\Mapping as ORM;
 
